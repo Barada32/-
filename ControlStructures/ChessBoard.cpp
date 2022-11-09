e48a25983a1863698beb695f43a919fee7faa1b4
hp820 <hp820@DESKTOP-3ULOMJB>
2022-11-09 17:44:30 +0300

    Добавьте файлы проекта.

---
+++ ControlStructures/ChessBoard.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+using namespace std;
+
+#define UPPER_LEFT_ANGLE	(char)218
+#define UPPER_RIGHT_ANGLE	(char)191
+#define LOWER_LEFT_ANGLE	(char)192
+#define LOWER_RIGHT_ANGLE	(char)217
+#define HORIZONTAL_LINE		(char)196
+#define VERTICAL_LINE		(char)179
+#define WHITE_BOX			"\xDB\xDB"//(char)219
+#define BLACK_BOX			"\x20\x20"
+//		\xXX - ïîçâîëÿåò çàäàòü Hex-êîä (XX) ASCII-ñèìâîëà
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	/*for (int i = 176; i < 224; i++)
+	{
+		if (i % 16 == 0)cout << endl;
+		cout << i << "\t" << (char)i << "\n";
+	}
+	cout << endl;*/
+	int n;
+	cout << "Ââåäèòå ðàçìåð äîñêè: "; cin >> n;
+	setlocale(LC_ALL, "C");
+	n++;
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <= n; j++)
+		{
+			if (i == 0 && j == 0)cout << UPPER_LEFT_ANGLE;
+			else if (i == 0 && j == n)cout << UPPER_RIGHT_ANGLE;
+			else if (i == n && j == 0)cout << LOWER_LEFT_ANGLE;
+			else if (i == n && j == n)cout << LOWER_RIGHT_ANGLE;
+			else if (i == 0 || i == n)cout << HORIZONTAL_LINE << HORIZONTAL_LINE;
+			else if (j == 0 || j == n)cout << VERTICAL_LINE;
+			else //cout << "  ";
+			{
+				/*if ((i + j) % 2 == 0)cout << WHITE_BOX << WHITE_BOX;
+				else cout << BLACK_BOX << BLACK_BOX;*/
+				//(i + j) % 2 == 0 ? cout << WHITE_BOX << WHITE_BOX : cout << BLACK_BOX << BLACK_BOX;
+				cout << ((i + j) % 2 == 0 ? WHITE_BOX : BLACK_BOX);
+			}
+		}
+		cout << endl;
+	}
+}
---
+++ КовтунОлегС++/main.cpp
@@ -0,0 +1,54 @@
+
+#include<iostream>
+using namespace std;
+
+int Add(int a, int b);	//Прототип функции (Объявление функции - Function declaration)
+int Sub(int a, int b);
+int Mul(int a, int b);
+int Div(int a, int b);
+double Pow(double a, double b);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a, b;
+	cout << "Введите два числа: "; cin >> a >> b;
+	int c = Add(a, b);	//Использование функции (Вызов функции - Function call)
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Sub(a, b) << endl;
+	cout << a << " * " << b << " = " << Mul(a, b) << endl;
+	cout << a << " / " << b << " = " << Div(a, b) << endl;
+	cout << a << " ^ " << b << " = " << Pow(a, b) << endl;
+
+}
+
+int Add(int a, int b)	//Реализация функции (Определение функции - Function definition)
+{
+	//Addition - сложение
+	int sum = a + b;
+	return sum;
+}
+int Sub(int a, int b)
+{
+	//Subtraction - вычитание
+	return a - b;
+}
+int Mul(int a, int b)
+{
+	//Multiplication - умножение
+	return a * b;
+}
+int Div(int a, int b)
+{
+	//Division - деление
+	return a / b;
+}
+double Pow(double a, double b)
+{
+	double chislo = a;
+	for (size_t i = 0; i < b - 1; i++)
+	{
+		chislo *= a;
+	}
+	return chislo;
+}
