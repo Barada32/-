58b737ade3315fd540434a2314f1af6824bc3a91
Дмитрий Кощеев <barada.90@yandex.ru>
2022-11-09 18:30:32 +0300

    Изменение функций

--- КовтунОлегС++/main.cpp
+++ КовтунОлегС++/main.cpp
@@ -6,13 +6,16 @@ int Add(int a, int b);	//Прототип функции (Объявление функции - Function declara
 int Sub(int a, int b);
 int Mul(int a, int b);
 int Div(int a, int b);
-double Pow(double a, double b);
+double Pow(double a, int b);
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	int a, b;
-	cout << "Введите два числа: "; cin >> a >> b;
+	cout << "Введите два число 1: ";
+	cin >> a;
+	cout << "Введите два число 2: ";
+	cin >> b;
 	int c = Add(a, b);	//Использование функции (Вызов функции - Function call)
 	cout << a << " + " << b << " = " << c << endl;
 	cout << a << " - " << b << " = " << Sub(a, b) << endl;
@@ -43,7 +46,7 @@ int Div(int a, int b)
 	//Division - деление
 	return a / b;
 }
-double Pow(double a, double b)
+double Pow(double a, int b)
 {
 	double chislo = a;
 	for (size_t i = 0; i < b - 1; i++)
